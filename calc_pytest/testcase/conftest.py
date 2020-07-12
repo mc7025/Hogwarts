d13eeaa73702f7e068ae012d2ce18b355469152d
mc7025 <232039123@qq.com>
2020-07-12 15:46:02 +0800

    update the test path and generate html report

--- calc_pytest/testcase/conftest.py
+++ calc_pytest/testcase/conftest.py
@@ -1,9 +1,8 @@
 import pytest
-import calc_pytest.src.calculator as calc
 
 
 @pytest.fixture(autouse=True)
-def s_t_fun(request):
+def s_t_fun():
     print("计算开始")
     yield
     print("计算结束")
--- calc_pytest/testcase/test_calc.py
+++ calc_pytest/testcase/test_calc.py
@@ -1,6 +1,12 @@
 import pytest
+import sys
+import os
+BASE_PATH = os.path.basename(os.path.abspath(__file__))
+sys.path.append(os.pardir)
+
 import calc_pytest.src.calculator as calc
 import yaml
+
 
 
 class TestCalc(object):
@@ -14,7 +20,8 @@ class TestCalc(object):
     # ], ids=["test1", "test2", "test3"])
     @pytest.mark.parametrize("a, b , result",
                              yaml.safe_load(
-                                 open("../testdata/datas.yml", encoding='utf-8')
+                                 open(os.path.join(BASE_PATH, "../testdata/datas.yml"),
+                                      encoding='utf-8')
                              ).get("test_data_add"))
     @pytest.mark.add
     def test_add(self, a, b, result):
@@ -22,7 +29,8 @@ class TestCalc(object):
 
     @pytest.mark.parametrize("a, b, result",
                              yaml.safe_load(
-                                 open("../testdata/datas.yml", encoding='utf-8')
+                                 open(os.path.join(BASE_PATH, "../testdata/datas.yml"),
+                                      encoding='utf-8')
                              ).get("test_data_sub"))
     @pytest.mark.sub
     def test_sub(self, a, b, result):
@@ -30,7 +38,8 @@ class TestCalc(object):
 
     @pytest.mark.parametrize("a, b, result",
                              yaml.safe_load(
-                                 open("../testdata/datas.yml", encoding='utf-8')
+                                 open(os.path.join(BASE_PATH, "../testdata/datas.yml"),
+                                      encoding='utf-8')
                              ).get("test_data_mul"))
     @pytest.mark.mul
     def test_mul(self, a, b, result):
@@ -38,7 +47,8 @@ class TestCalc(object):
 
     @pytest.mark.parametrize("a, b, result",
                              yaml.safe_load(
-                                 open("../testdata/datas.yml", encoding='utf-8')
+                                 open(os.path.join(BASE_PATH, "../testdata/datas.yml"),
+                                      encoding='utf-8')
                              ).get("test_data_div"))
     @pytest.mark.div
     def test_div(self, a, b, result):
