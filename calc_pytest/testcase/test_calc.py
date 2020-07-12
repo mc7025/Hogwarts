c201a8f7b3c7836bb5a7ecd66d2eef085868b87a
mc7025 <232039123@qq.com>
2020-07-12 16:13:53 +0800

    update ids

--- calc_pytest/testcase/test_calc.py
+++ calc_pytest/testcase/test_calc.py
@@ -1,11 +1,11 @@
 import pytest
 import sys
 import os
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.pardir)
-
 import calc_pytest.src.calculator as calc
 import yaml
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_data(filename=os.path.join(BASE_PATH, "../testdata/datas.yml")):
@@ -24,25 +24,29 @@ class TestCalc(object):
     #     (2, 2, 4),
     # ], ids=["test1", "test2", "test3"])
     @pytest.mark.parametrize("a, b , result",
-                             get_data().get("test_data_add"))
+                             get_data().get("test_data_add"),
+                             ids=[f"add_case{x+1}" for x in range(len(get_data().get("test_data_add")))])
     @pytest.mark.add
     def test_add(self, a, b, result):
         assert result == round(self.calc.add(a, b), 2)
 
     @pytest.mark.parametrize("a, b, result",
-                             get_data().get("test_data_sub"))
+                             get_data().get("test_data_sub"),
+                             ids=[f"sub_case{x+1}" for x in range(len(get_data().get("test_data_sub")))])
     @pytest.mark.sub
     def test_sub(self, a, b, result):
         assert result == round(self.calc.sub(a, b), 2)
 
     @pytest.mark.parametrize("a, b, result",
-                             get_data().get("test_data_mul"))
+                             get_data().get("test_data_mul"),
+                             ids=[f"mul_case{x+1}" for x in range(len(get_data().get("test_data_mul")))])
     @pytest.mark.mul
     def test_mul(self, a, b, result):
         assert result == round(self.calc.mul(a, b), 2)
 
     @pytest.mark.parametrize("a, b, result",
-                             get_data().get("test_data_div"))
+                             get_data().get("test_data_div"),
+                             ids=[f"div_case{x+1}" for x in range(len(get_data().get("test_data_div")))])
     @pytest.mark.div
     def test_div(self, a, b, result):
         if b == 0:
